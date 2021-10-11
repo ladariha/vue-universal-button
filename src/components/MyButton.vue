8e6d39ec859b134e4a79daa3d69f9ec8dc0abffe
Vladimir Riha <vladimir.riha@memsource.com>
2021-10-11 09:29:26 +0200

    Distribute button as source

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,6 +1,8 @@
 <template>
-  <button @click="onClick">Click me</button>
-  <h2>Click count {{ clickCount }}</h2>
+  <div>
+    <button @click="onClick">Click me</button>
+    <h2>Click count {{ clickCount }}</h2>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue-demi";
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import MyButton from "@/components/MyButton.vue";
 
-describe("HelloWorld.vue", () => {
+describe("MyButton.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
+    const msg = "Click meClick count 0";
+    const wrapper = shallowMount(MyButton);
     expect(wrapper.text()).toMatch(msg);
   });
 });
